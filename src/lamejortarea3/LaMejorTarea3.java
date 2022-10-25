8e51aa49cf9d8d09655baca3e60ce71bd1ae89f0
avillagran801 <96851076+avillagran801@users.noreply.github.com>
2022-10-25 10:39:35 -0300

    Mi primera ventana :D

---
+++ src/lamejortarea3/LaMejorTarea3.java
@@ -0,0 +1,11 @@
+package lamejortarea3;
+import java.awt.*;
+import javax.swing.*;
+
+public class LaMejorTarea3 {
+
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+    }
+    
+}
---
+++ src/lamejortarea3/Ventana.java
@@ -0,0 +1,23 @@
+package lamejortarea3;
+import java.awt.*;
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+    public Ventana(){
+        super();
+        this.setLayout(new BorderLayout());
+        this.setTitle("Tarea 3");
+        
+        this.add(new JButton("sur"),BorderLayout.SOUTH);
+        this.add(new JButton("centro"),BorderLayout.CENTER);
+        this.add(new JButton("norte"),BorderLayout.NORTH);
+        this.add(new JButton("este"),BorderLayout.EAST);
+        this.add(new JButton("oeste"),BorderLayout.WEST);
+        
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(1000,800); 
+        this.setVisible(true); 
+
+    }
+    
+}
