813d9a94c97a851670a9fa3ade9b83ec3c423c3d
Pepancho <joscortes2021@udec.cl>
2022-11-12 14:38:21 -0300

    Corrección: Eliminación de atributos no usados

--- src/lamejortarea3/Expendedor.java
+++ src/lamejortarea3/Expendedor.java
@@ -12,7 +12,6 @@ public class Expendedor extends JPanel{
     private DepositoCompra compra;
     private DepositoVuelto vuelto;
     private ArrayList<Moneda> monedas;
-    private JLabel emptyDeposit;
     
     public Expendedor(int cant, int precio_aux){
         precio = precio_aux;
@@ -36,10 +35,6 @@ public class Expendedor extends JPanel{
            sprite.addBebida(aux_sprite);
            fanta.addBebida(aux_fanta);
         }
-    }
-    
-    public Boolean sinBebida(){
-        return coca.isEmpty()||sprite.isEmpty()||fanta.isEmpty();
     }
     
     @Override
--- src/lamejortarea3/PanelPrincipal.java
+++ src/lamejortarea3/PanelPrincipal.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 public class PanelPrincipal extends JPanel{
     private Comprador con;
     private Expendedor exp;
-    private Moneda1000 m1000;
     
     public PanelPrincipal(){
         exp = new Expendedor (6, 1000);
